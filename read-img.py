19e925e4ba6026698b82d69d1bf4de89b7297451
recalldude <thelava12@gmail.com>
2022-04-07 12:18:02 +0200

    new maj

--- read-img.py
+++ read-img.py
@@ -14,16 +14,15 @@ def approx(A,k):
     U, S, VT = np.linalg.svd(A ,full_matrices=False)
     S = np.diag(S)    
     approx = U[:,:k] @ S[:k,:k] @ VT[:k,:]
+    print('ok+1 :', S[k+1, k+1])
     return approx.astype(int)
-
-
-
 
 
 def makeAnApprox(imgPath, krange):
     fig = plt.figure()
     img, R, G, B, h, w = openImage(imgPath)
     j = 0
+    long = len(krange) + 1
     for i in krange:
         j+=1
         approxR = approx(R, i)
@@ -33,12 +32,18 @@ def makeAnApprox(imgPath, krange):
         approxRGB[:,:,0] = np.copy(approxR)
         approxRGB[:,:,1] = np.copy(approxG)
         approxRGB[:,:,2] = np.copy(approxB)
-        fig.add_subplot(2, 2, j)
+        fig.add_subplot(1, long, j)
         plt.imshow(approxRGB)
         plt.axis("off")
         plt.title("k =" + str(i))
-
+    fig.add_subplot(1,long,j+1)
+    plt.imshow(img)
+    plt.axis("off")
+    plt.title("originale")
     return (img, approxRGB)
+
+
+    # def psnr(img, approx):
     # j = 0
     # for r in k:
     
@@ -52,8 +57,8 @@ def makeAnApprox(imgPath, krange):
 
 k = (1, 2, 3)
 print(len(k))
-img, approxRGB = makeAnApprox('chat.jpg', (5, 20, 50))
-# plt.imshow(approxRGB[:,:,:])
+img, approxRGB = makeAnApprox('chat.jpg', (5, 20, 50, 100))
+
 plt.show()
 
 
